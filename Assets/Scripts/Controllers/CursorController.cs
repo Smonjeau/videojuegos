52ff3ec094be67a50e4b9b9a3314d643d4376224
inesmarca <imarcarian@itba.edu.ar>
2022-11-18 11:48:08 -0300

    change ilumination, changed aim and changed grenade collision

---
+++ Assets/Scripts/Controllers/CursorController.cs
@@ -0,0 +1,41 @@
+using Managers;
+using UI;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class CursorController : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+        private void Update()
+        {
+            bool gamePaused = GlobalData.Instance.GamePaused;
+            switch (gamePaused)
+            {
+                case true when Cursor.lockState == CursorLockMode.Locked:
+                    Cursor.lockState = CursorLockMode.None;
+                    break;
+                case false:
+                {
+                    if (Input.GetKeyDown(KeyCode.Escape))
+                    {
+                        Cursor.lockState = CursorLockMode.None;
+                    }
+            
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Cursor.lockState = CursorLockMode.Locked;
+                    }
+
+                    break;
+                }
+            }
+        }
+        
+    }
+}
--- Assets/Scripts/Entities/Grenade.cs
+++ Assets/Scripts/Entities/Grenade.cs
@@ -1,3 +1,4 @@
+using System;
 using Controllers;
 using Strategy;
 using UnityEngine;
@@ -7,26 +8,14 @@ namespace Entities
     public class Grenade : MonoBehaviour
     {
         [SerializeField] private GameObject _eplosionPrefab;
-        [SerializeField] private float _delay = 3f;
         [SerializeField] private float _radius = 10f;
         [SerializeField] private float _force = 800f;
         [SerializeField] private int _damage = 50;
-        private float _countdown;
-        private bool _hasExploded;
 
-        void Start()
-        {
-            _countdown = _delay;
-        }
 
-        void Update()
+        private void OnTriggerEnter(Collider other)
         {
-            _countdown -= Time.deltaTime;
-            if (_countdown <= 0 && !_hasExploded)
-            {
-                _hasExploded = true;
-                Explode();
-            }
+            Explode();
         }
 
         private void Explode()
@@ -39,7 +28,7 @@ namespace Entities
                 Rigidbody rb = collider.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
-                    rb.AddExplosionForce(_force, transform.position, _radius); // hace que se mueva por culpa de la explosion
+                    rb.AddExplosionForce(_force, transform.position, _radius);
                 }
 
                 IDamageable damageable = collider.GetComponent<IDamageable>();
